ada7d9d4c09f54df522542aaeb9559f1600e31b2
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-08-21 09:27:50 +0500

    Create q3.py

---
+++ Labs/01/q3.py
@@ -0,0 +1,15 @@
+#Write a program to take an integer list input from user and count all the even numbersin that list and print the count.
+
+num = int(input("Enter the number of integers for the list: "))
+
+user_input = list(map(int, input("Enter the integers for the list separated by spaces: ").split()))
+
+count = 0
+
+for i in range(num):
+    if user_input[i] % 2 == 0:
+        count += 1
+
+print("Count of even numbers:", count)
+
+
