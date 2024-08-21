8ed48d502bc8f1531dd0c1989029f472d5c9d62d
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-08-21 10:02:54 +0500

    Update q3.py

--- Labs/01/q3.py
+++ Labs/01/q3.py
@@ -1,13 +1,21 @@
 #Write a program to take an integer list input from user and count all the even numbersin that list and print the count.
 
-num = int(input("Enter the number of integers for the list: "))
-
-user_input = list(map(int, input("Enter the integers for the list separated by spaces: ").split()))
+numbers = []
 
 count = 0
 
+num = int(input("Enter the number of integers for the list: "))
+
 for i in range(num):
-    if user_input[i] % 2 == 0:
+    user_input = int(input("Enter Number: "))
+    numbers.append(user_input)
+
+for number in numbers:
+    if number % 2 == 0:
         count += 1
 
 print("Count of even numbers:", count)
+
+
+
+
