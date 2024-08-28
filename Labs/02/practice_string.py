ad7cfe0c51aff4698d1f3ad8e04a5bd69dd2342b
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-08-28 09:51:09 +0500

    Create practice_string.py

---
+++ Labs/02/practice_string.py
@@ -0,0 +1,8 @@
+name = input("Enter your good name!: ")
+print("Hello !",name)
+share = input("I hope you are fine, let me know how I can help you! (yes or no): ")
+if share=='yes':
+   problem = input("share your problem with us: ")
+   print("Thanks for your feedback, we will resolve your problem") 
+else:
+    print("Okay! Good to see you , stay connected")        
