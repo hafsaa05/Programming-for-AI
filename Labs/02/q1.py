522fb39eca95eda52d592980c3d9a2316311b271
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-08-28 09:22:46 +0500

    Update q1.py

--- Labs/02/q1.py
+++ Labs/02/q1.py
@@ -1,3 +1,4 @@
+import math
 
 def trapezoid(x, y, z):
     print('Area of trapezoid: ' ,((x+y)*z)/2)
@@ -6,8 +7,8 @@ def Parallelogram(a, b):
     print("Area of Parallelogram: " ,(a*b))
 
 def cylinder(r, h):
-    print("Volume of cylinder: ", (3.142*r*r*h))
-    print("Surface area of cylinder: ", (2*3.142*r * (r+h)))
+    print("Volume of cylinder: ", (math.pi*r*r*h))
+    print("Surface area of cylinder: ", (2*math.pi*r * (r+h)))
 
 trapezoid(6, 6, 8)
 Parallelogram(7, 9)
