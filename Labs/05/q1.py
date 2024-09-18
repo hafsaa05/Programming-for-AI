51d5998e62049d59bc7e1aab114691c0cf282725
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-09-18 08:37:36 +0500

    Create q1.py

---
+++ Labs/05/q1.py
@@ -0,0 +1,19 @@
+class Vehicle:
+    def __init__ (self, capacity):
+        self.capacity = capacity
+    
+    def fare(self):
+        Fare = self.capacity*100
+        return Fare
+        
+class Bus(Vehicle):
+    def __init__ (self, capacity):
+        self.capacity = capacity
+        
+    def bus_fare(self):
+        Bus_fare = self.fare() + (self.fare()*0.010)
+        print(f"Total fare: {Bus_fare}")
+        
+bus = Bus(7)
+print(bus.fare())
+bus.bus_fare()
