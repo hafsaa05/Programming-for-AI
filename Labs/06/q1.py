76a5964a805a256bdecfbb769cf74447ab5aefd6
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-09-25 09:23:42 +0500

    Update q1.py

--- Labs/06/q1.py
+++ Labs/06/q1.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
-data = {
-    'movies': ['Deadpool', 'Pride & Prejudice', 'Glassworker', 'Harry Potter'],
-    'revenue': [25000000, 15000000, 5000000, 1200000],
-    'budget': [800000, 1200000, 950000, 700000]
-}
-
-df = pd.DataFrame(data)
+df = pd.read_csv('movies.csv')
 
 filtered = df[(df['revenue'] > 2000000) & (df['budget'] < 1000000)]
-
 print(filtered)
