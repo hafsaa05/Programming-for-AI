1a0f2dd69981ddf0d9b482987177f71113a0a49c
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-09-25 09:56:37 +0500

    Update q2.py

--- Labs/06/q2.py
+++ Labs/06/q2.py
@@ -1,7 +1,11 @@
 import pandas as pd
+data = {
+    'movies': ['Deadpool', 'Pride & Prejudice', 'Glassworker', 'Harry Potter'],
+    'revenue': [25000000, 15000000, 5000000, 1200000],
+    'budget': [800000, 1200000, 950000, 700000]
+}
 
-df = pd.read_csv('movies2.csv')
-
-sort = df.sort_values(by='runtime', ascending=False)
+df = pd.DataFrame(data)
+sort = df.sort_values(by='budget' , ascending=False)
 
 print(sort)
