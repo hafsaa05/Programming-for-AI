45cbf09e2d25675db6111ddf2b5f73670baef491
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-09-25 09:15:27 +0500

    Create q1.py

---
+++ Labs/06/q1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    'movies': ['Deadpool', 'Pride & Prejudice', 'Glassworker', 'Harry Potter'],
+    'revenue': [25000000, 15000000, 5000000, 1200000],
+    'budget': [800000, 1200000, 950000, 700000]
+}
+
+df = pd.DataFrame(data)
+
+filtered = df[(df['revenue'] > 2000000) & (df['budget'] < 1000000)]
+
+print(filtered)
