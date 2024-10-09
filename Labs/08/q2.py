2d738686347d54b5a4603fefe4c7e70c11bf99bf
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-10-09 09:01:04 +0500

    Update q2.py

--- Labs/08/q2.py
+++ Labs/08/q2.py
@@ -1,11 +1,10 @@
 # Create a multi-dimensional array of 3 rows and 3 columns having odd numbers from 1 to
 # 19 including 1 and excluding 19. After that, iterate over this array to print all elements.
 import numpy as np
-arr = np.arange(1, 19, 2)
-new_arr = arr.reshape(3, 3)
-print(f'Array: {new_arr}')
+arr = np.arange(1, 19, 2).reshape(3, 3)
+print(f'Array: {arr}')
 
 print("\nElements in the array:")
-for r in new_arr:
+for r in arr:
     for e in r:
         print(e)
