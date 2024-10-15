31e0e829231f99fe0f580f08eed3198ff7f63bef
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-10-16 04:31:25 +0500

    Update q5.py

--- Labs/08/q5.py
+++ Labs/08/q5.py
@@ -8,10 +8,10 @@ new_arr = np.random.choice(arr, size=(4, 4))
 print(new_arr)
 
 identity = np.eye(4,4)
-res = new_arr*identity
+res = np.multiply(new_arr, identity)
 print(f'Multiplying with identity matrix: \n{res}')
 
 arr2 = np.arange(1, 33, 2).reshape(4, 4)
-res2 = np.add(res,arr2)
 print(arr2)
+res2 = np.add(res,arr2)
 print(f'Adding matrices: \n{res2}')
