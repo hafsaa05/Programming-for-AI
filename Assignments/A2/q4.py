a1edaf0a2ab95e7291d48d01b0c7ffe912b2f830
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-11-01 10:03:28 +0500

    Create q4.py

---
+++ Assignments/A2/q4.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr1 = np.arange(1,10)
+arr2 = np.arange(11,20)
+print(f'Addition : {np.add(arr1,arr2)}')
+print(f'Subtraction : {np.subtract(arr1,arr2)}')
+print(f'Multiply : {np.multiply(arr1,arr2)}')
+print(f'Division : {np.divide(arr1,arr2)}')
