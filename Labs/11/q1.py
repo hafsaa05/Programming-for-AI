6da54834e950f81df5a38329fb613a48fad1d76a
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-11-13 09:50:27 +0500

    Update q1.py

--- Labs/11/q1.py
+++ Labs/11/q1.py
@@ -1,1 +1,10 @@
-.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier  # Corrected import
+
+# Load the dataset
+ds = pd.read_csv("dermatology.data")
+print(ds)
