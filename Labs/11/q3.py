ca88cf19dbeb049ffa557eb0c0a2767edf362a13
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-11-18 08:24:28 +0500

    Update q3.py

--- Labs/11/q3.py
+++ Labs/11/q3.py
@@ -1,1 +1,9 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
 
+data = pd.read_csv('dermatology.data')
